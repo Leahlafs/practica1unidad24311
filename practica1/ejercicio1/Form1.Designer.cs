02012691d09d1b7524400f66a856a944d210fb24
Leahlafs <leydianafrias83@gmail.com>
2025-06-10 13:10:38 -0400

    agregando label

---
+++ practica1/ejercicio1/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ejercicio1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 10F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(29, 106);
+            label1.Name = "label1";
+            label1.Size = new Size(573, 23);
+            label1.TabIndex = 0;
+            label1.Text = "!Bienvenido al formulario de Leydi Ana Frias LR-2024-01393\r\n";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(632, 236);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Bienvenida";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+    }
+}
